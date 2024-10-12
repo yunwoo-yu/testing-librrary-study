5b614eac625f2ccc2eb2edfb2b113eb9e0e73b75
Ycow <skypnal12@gmail.com>
2024-10-12 23:50:49 +0900

    feat: button text,color change flow testing

--- my-color-button/src/App.jsx
+++ my-color-button/src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const nextColor = buttonColor === "red" ? "blue" : "red";
+
+  const onClickButton = () => {
+    setButtonColor(nextColor);
+  };
+
   return (
     <div>
-      <button className="red">Change to blue</button>
+      <button className={buttonColor} onClick={onClickButton}>
+        Change to {nextColor}
+      </button>
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={false}
+      />
+      <label htmlFor="disable-button-checkbox">Disable Button</label>
     </div>
   );
 }
--- my-color-button/src/App.test.jsx
+++ my-color-button/src/App.test.jsx
@@ -1,12 +1,27 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("button starts with correct color", () => {
+test("button click flow", () => {
   render(<App />);
+
   const button = screen.getByRole("button", { name: /blue/i });
 
   expect(button).toHaveClass("red");
+
+  fireEvent.click(button);
+
+  expect(button).toHaveTextContent(/red/i);
+
+  expect(button).toHaveClass("blue");
 });
 
-test("button has correct color after click", () => {});
-test("button has correct text after click", () => {});
+test("checkbox flow", () => {
+  render(<App />);
+
+  const button = screen.getByRole("button", { name: /blue/i });
+  const checkbox = screen.getByRole("checkbox", { name: /disable button/i });
+
+  expect(button).toBeEnabled();
+
+  expect(checkbox).not.toBeChecked();
+});
