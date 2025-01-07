0125f9e8e5190c38c2823fe0841f4c3a722a2c32
Ycow <skypnal12@gmail.com>
2025-01-07 23:26:54 +0900

    feat: testing code add

---
+++ section-5/src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+
+import Options from "../Options";
+
+test("아이스크림 종류 별로 이미지를 표시한다.", () => {
+  render(<Options optionType="scoops" />);
+
+  // 이미지를 찾는다.
+  const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+
+  // 이미지의 개수를 확인한다.
+  expect(scoopImages).toHaveLength(2);
+
+  // 이미지의 alt 속성을 확인한다.
+  const altText = scoopImages.map((el) => el.alt);
+
+  // 이미지의 alt text가 일치하는지 확인한다.
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
