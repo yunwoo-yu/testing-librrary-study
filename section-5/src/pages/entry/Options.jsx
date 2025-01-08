e791cda6c7032392fd21f4cd75b74d34897d13d4
Ycow <skypnal12@gmail.com>
2025-01-09 00:11:26 +0900

    feat: Option UI, test add (With Scoops)

---
+++ section-5/src/pages/entry/Options.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ScoopOption from "./ScoopOption";
+import Row from "react-bootstrap/Row";
+
+const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((error) => {
+        // 에러처리는 나중에처리
+        console.log(error);
+      });
+  }, [optionType]);
+
+  // 토핑 옵션 생성 후 null 변경
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+};
+
+export default Options;
--- section-5/src/pages/entry/tests/Options.test.jsx
+++ section-5/src/pages/entry/tests/Options.test.jsx
@@ -2,11 +2,11 @@ import { render, screen } from "@testing-library/react";
 
 import Options from "../Options";
 
-test("아이스크림 종류 별로 이미지를 표시한다.", () => {
+test("아이스크림 종류 별로 이미지를 표시한다.", async () => {
   render(<Options optionType="scoops" />);
 
   // 이미지를 찾는다.
-  const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole("img", { name: /scoops$/i });
 
   // 이미지의 개수를 확인한다.
   expect(scoopImages).toHaveLength(2);
@@ -15,5 +15,5 @@ test("아이스크림 종류 별로 이미지를 표시한다.", () => {
   const altText = scoopImages.map((el) => el.alt);
 
   // 이미지의 alt text가 일치하는지 확인한다.
-  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+  expect(altText).toEqual(["Chocolate scoops", "Vanilla scoops"]);
 });
